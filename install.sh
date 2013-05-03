c53c6484c36d7a8ce054edaafb68ce8b60db62a3
DrupalInCloud <r.agabekov@initlab.ru>
2013-05-03 23:41:09 +0400

    Change install.sh (add pacckages installation)

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 # sync our configuration with /etc
+aptitude install rsync apache2 mysql-server nginx memcached php5-memcached php5-gd
 
 rsync -av ./apache2/ /etc/apache2/
 rsync -av ./mysql/ /etc/mysql/
 rsync -av ./nginx/ /etc/nginx/
 rsync -av ./php5/ /etc/php5/
 cp ./memcached.conf /etc/memcached.conf
+
+mkdir /var/www/default
